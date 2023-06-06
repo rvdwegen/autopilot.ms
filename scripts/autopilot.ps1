48136563b0bc0ffcd58ba12029f12190029c4a02
Roel van der Wegen <github@aeternus.tech>
2023-06-06 18:04:42 +0200

    Add files via upload

---
+++ scripts/autopilot.ps1
@@ -0,0 +1,39 @@
+$ProgressPreference = 'SilentlyContinue'
+CLS
+
+$script = @"
+                                                
+       _         _              _ _       _     ____              _       _                   
+      / \  _   _| |_ ___  _ __ (_) | ___ | |_  | __ )  ___   ___ | |_ ___| |_ _ __ __ _ _ __  
+     / _ \| | | | __/ _ \| '_ \| | |/ _ \| __| |  _ \ / _ \ / _ \| __/ __| __| '__/ _`  | '_ \ 
+    / ___ \ |_| | || (_) | |_) | | | (_) | |_  | |_) | (_) | (_) | |_\__ \ |_| | | (_| | |_) |
+   /_/   \_\__,_|\__\___/| .__/|_|_|\___/ \__| |____/ \___/ \___/ \__|___/\__|_|  \__,_| .__/ 
+                         |_|                                                           |_|    
+                                                 
+                                                
+                      ============ Autopilot hash bootstrap ============                      
+                                  Author: Roel van der Wegen
+
+"@
+
+Write-Host $script
+
+function Save-File ([string]$filename) {
+    [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+
+    $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+    $OpenFileDialog.initialDirectory = "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
+    $OpenFileDialog.filter = 'CSV (*.csv)|*.csv'
+    $OpenFileDialog.FileName = "$filename.csv"
+    [void]$OpenFileDialog.ShowDialog()
+
+    return $OpenFileDialog.filename
+}
+
+$SerialNumber = (Get-WmiObject win32_bios | select Serialnumber).SerialNumber
+
+Write-Host "Installing dependencies..."
+Install-Script -name Get-WindowsAutopilotInfo -Force
+
+Write-Host "Retrieving Autpilot Hash..."
+Get-WindowsAutopilotInfo -OutputFile (Save-File -filename $SerialNumber)
