8613a0661f10f8249b2956b11adf1cdda1452eed
Roel van der Wegen <github@aeternus.tech>
2023-12-03 06:34:45 +0100

    Third option basics

--- scripts/tobenamedlater.ps1
+++ scripts/tobenamedlater.ps1
@@ -17,11 +17,52 @@ $header = @"
 
 "@
 
-CLS
+Clear-Host
 
 Write-Host $header
 
-# Install MSAL.PS
-@('MSAL.PS') | ForEach-Object {
+try {
+       # Change execution policy
+       if (Get-ExecutionPolicy -ne "Unrestricted") {
+              Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -Confirm:$false
+              Write-Host "Executionpolicy has been changed to unrestricted for this process" -ForegroundColor Green
+       }
 
+       # Install PackageProvider NuGet
+       if (!(Get-PackageProvider | Where-Object { $_.Name -eq "NuGet" })) {
+              Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$FALSE | Out-Null
+       }
+
+       # Check & Install requisite modules
+       $installedModules = Get-InstalledModule
+       Write-Host "Checking modules..."
+       @('Microsoft.Graph.Intune', 'Microsoft.Graph.Groups', 'Microsoft.Graph.Authentication','MSAL.PS', 'WindowsAutoPilotIntune') | ForEach-Object {
+              try {
+                     if ($installedModules.Name -notcontains $($_)) {
+                            Install-Module -Name $($_) -Force -Confirm:$FALSE
+                            Write-Host "Module $($_) has been installed" -ForegroundColor Green
+                     } else {
+                            Write-Host "Module $($_) has been found" -ForegroundColor Green
+                     }
+              } catch {
+                     Write-Error "Failed to install/find modules: $($_)"
+              }
+       }
+} catch {
+       throw "$($_)"
 }
+
+try {
+       # Validate tenant
+       try {
+              $tenant = Read-Host "Please enter the tenantId or a valid domain of the tenant"
+              $tenantId = (Invoke-RestMethod -Method GET "https://login.windows.net/$tenant/.well-known/openid-configuration").token_endpoint.Split('/')[3]
+       } catch {
+              throw "Tenant $($tenant) could not be found"
+       }
+
+       Connect-MgGraph -ClientId 'd1ddf0e4-d672-4dae-b554-9d5bdfd93547' -TenantId $tenantId #-NoWelcome
+} catch {
+       throw "Authentication error: $($_.Exception.Message)"
+}
+
