538d6b29726cad20adcd49b91521c374229427ae
Roel van der Wegen <github@aeternus.tech>
2023-12-03 05:32:18 +0100

    Create tobenamedlater.ps1

---
+++ scripts/tobenamedlater.ps1
@@ -0,0 +1,27 @@
+$ProgressPreference = 'SilentlyContinue'
+$host.ui.RawUI.WindowTitle = "Autopilot Hash Bootstrap"
+
+$header = @"
+                                                
+       _         _              _ _       _     ____              _       _                   
+      / \  _   _| |_ ___  _ __ (_) | ___ | |_  | __ )  ___   ___ | |_ ___| |_ _ __ __ _ _ __  
+     / _ \| | | | __/ _ \| '_ \| | |/ _ \| __| |  _ \ / _ \ / _ \| __/ __| __| '__/ _`  | '_ \ 
+    / ___ \ |_| | || (_) | |_) | | | (_) | |_  | |_) | (_) | (_) | |_\__ \ |_| | | (_| | |_) |
+   /_/   \_\__,_|\__\___/| .__/|_|_|\___/ \__| |____/ \___/ \___/ \__|___/\__|_|  \__,_| .__/ 
+                         |_|                                                           |_|    
+                                                 
+                                                
+                      ============ Autopilot hash bootstrap ============                      
+                             Author: https://github.com/rvdwegen
+                        Repo: https://github.com/rvdwegen/autopilot.ms
+
+"@
+
+CLS
+
+Write-Host $header
+
+# Install MSAL.PS
+@('MSAL.PS') | ForEach-Object {
+
+}
