db1feeb6df904ed7fee2752e576b2fd7d684e45c
Roel van der Wegen <github@aeternus.tech>
2023-12-07 12:08:45 +0100

    Update postoobe.ps1

--- scripts/postoobe.ps1
+++ scripts/postoobe.ps1
@@ -45,7 +45,8 @@ try {
 try {
     Write-Host "ATTEMPTING TO INSTALL ALL WINDOWS UPDATES" -ForegroundColor DarkYellow
     Write-Host "THIS MAY TAKE A WHILE..." -ForegroundColor DarkYellow
-    Get-WindowsUpdate -Install -AcceptAll -AutoReboot -RecurseCycle 3 -Confirm:$false
+    #Get-WindowsUpdate -Install -AcceptAll -AutoReboot -RecurseCycle 3 -Confirm:$false
+    Get-WindowsUpdate -Install -AcceptAll -RecurseCycle 3 -Confirm:$false
 
     if ((Get-WURebootStatus).RebootRequired -eq $true) {
         # Schedule a one time start of get-windowsupdate after reboot
