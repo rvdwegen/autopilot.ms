fa8594c047c27665b12cb92a763821b39b9bccb4
Roel van der Wegen <github@aeternus.tech>
2024-06-06 00:13:29 +0200

    Create tobenamedlater2.ps1

---
+++ scripts/tobenamedlater2.ps1
@@ -0,0 +1,90 @@
+$ProgressPreference = 'SilentlyContinue'
+$host.ui.RawUI.WindowTitle = "Autopilot Hash Bootstrap"
+
+$header = @"
+                                                
+       _         _              _ _       _     ____              _       _                   
+      / \  _   _| |_ ___  _ __ (_) | ___ | |_  | __ )  ___   ___ | |_ ___| |_ _ __ __ _ _ __  
+     / _ \| | | | __/ _ \| '_ \| | |/ _ \| __| |  _ \ / _ \ / _ \| __/ __| __| '__/ _`  | '_ \ 
+    / ___ \ |_| | || (_) | |_) | | | (_) | |_  | |_) | (_) | (_) | |_\__ \ |_| | | (_| | |_) |
+   /_/   \_\__,_|\__\___/| .__/|_|_|\___/ \__| |____/ \___/ \___/ \__|___/\__|_|  \__,_| .__/ 
+                         |_|                                                           |_|    
+                                                 
+                                                
+                      ============ Autopilot hash bootstrap ============                      
+                             Author: https://github.com/rvdwegen
+                        Repo: https://github.com/rvdwegen/autopilot.ms
+
+"@
+
+Clear-Host
+
+Write-Host $header
+
+#region
+
+function Save-File ([string]$fileName, [string]$folderPath) {
+       [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+   
+       $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+       $OpenFileDialog.initialDirectory = $folderPath
+       $OpenFileDialog.filter = 'JSON (*.json)|*.json'
+       $OpenFileDialog.FileName = "$fileName.json"
+       $result = $OpenFileDialog.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true }))
+   
+       if ($result -NE "OK") { throw "Failed to select save location" }
+       return [pscustomobject]@{
+           path = $OpenFileDialog.filename
+           status = $result
+       }
+}
+
+#endregion
+
+try {
+       # Validate tenant
+       try {
+              $tenant = Read-Host "Please enter the tenantId or a valid domain of the tenant"
+              $tenantId = (Invoke-RestMethod -Method GET "https://login.windows.net/$tenant/.well-known/openid-configuration").token_endpoint.Split('/')[3]
+       } catch {
+              throw "Tenant $($tenant) could not be found"
+       }
+} catch {
+       throw "Authentication error: $($_.Exception.Message)"
+}
+
+try {
+
+  $CloudAssignedAadServerData = @{
+      ZeroTouchConfig = @{
+          ForcedEnrollment = '1'
+          CloudAssignedTenantDomain = 'ShellCorp.onmicrosoft.com'
+          CloudAssignedTenantUpn = '\'
+      }
+  } | ConvertTo-Json
+
+  $Profile = @{
+      Version = 2049
+      CloudAssignedTenantId = "db1e96a8-a3da-442a-930b-235cac24cd5c"
+      CloudAssignedForcedEnrollment = 1
+      CloudAssignedDomainJoinMethod = 0
+      CloudAssignedAutopilotUpdateDisabled = 1
+      CloudAssignedAutopilotUpdateTimeout = 1800000
+      CloudAssignedOobeConfig = 1310
+      CloudAssignedAadServerData = $CloudAssignedAadServerData
+      CloudAssignedTenantDomain = "ShellCorp.onmicrosoft.com"
+      ZtdCorrelationId = (New-Guid).Guid
+      CloudAssignedDeviceName = "%SERIAL%"
+      Comment_File = "Profile Default"
+  }
+
+} catch {
+       throw "Failed to generate Autopilot JSON file: $($_.Exception.Message)"
+}
+
+try {
+       $saveFilePath = Save-File -fileName "AutoPilotConfigurationFile" -folderPath (Join-Path -Path $env:windir -ChildPath "Provisioning\Autopilot")
+       $profile | ConvertTo-Json -Depth 20 | Out-File -FilePath $saveFilePath.Path -Encoding ASCII
+} catch {
+       throw "Failed to save file: $($_.Exception.Message)"
+}
