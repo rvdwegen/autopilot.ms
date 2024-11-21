1687302f31a3f507c02dfd7d531ee1be0358de9e
Roel van der Wegen <github@aeternus.tech>
2024-11-21 10:16:37 +0100

    dfdfdf

--- scripts/gettenant.ps1
+++ scripts/gettenant.ps1
@@ -1,25 +1,6 @@
 $ProgressPreference = 'SilentlyContinue'
-$host.ui.RawUI.WindowTitle = "Autopilot Hash Bootstrap"
-
 Clear-Host
-
-$header = @"
-                                                
-       _         _              _ _       _     ____              _       _                   
-      / \  _   _| |_ ___  _ __ (_) | ___ | |_  | __ )  ___   ___ | |_ ___| |_ _ __ __ _ _ __  
-     / _ \| | | | __/ _ \| '_ \| | |/ _ \| __| |  _ \ / _ \ / _ \| __/ __| __| '__/ _`  | '_ \ 
-    / ___ \ |_| | || (_) | |_) | | | (_) | |_  | |_) | (_) | (_) | |_\__ \ |_| | | (_| | |_) |
-   /_/   \_\__,_|\__\___/| .__/|_|_|\___/ \__| |____/ \___/ \___/ \__|___/\__|_|  \__,_| .__/ 
-                         |_|                                                           |_|    
-                                                 
-                                                
-                      ============ Autopilot hash bootstrap ============                      
-                             Author: https://github.com/rvdwegen
-                        Repo: https://github.com/rvdwegen/autopilot.ms
-
-"@
-
-Write-Host $header
+Write-Host $env:logoheader
 
 try {
     $jsonRaw = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Provisioning\AutopilotPolicyCache").PolicyJsonCache
